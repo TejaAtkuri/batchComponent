0f9e2f5e2b0beeeb4b054ecc4b1eb6751a9020ca
TejaAtkuri <47035106+TejaAtkuri@users.noreply.github.com>
2023-08-28 22:43:44 +0530

    Batch skeleton

---
+++ bud/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import BatchComponent from "./BatchCompon/BatchComponent";
+
+function App() {
+  return <BatchComponent />;
+}
+
+export default App;
---
+++ bud/src/BatchCompon/BatchComponent.jsx
@@ -0,0 +1,96 @@
+import './BatchComponent.css'
+
+
+function BatchComponent(){
+
+    return(
+        <>
+        <h1 className='batchHeader'>Upcoming Batches</h1>
+        <div className="batchCard">
+            <div className="courseImageContainer">
+                <img src="temp"/>
+            </div>
+            <div className="courseNameContainer">
+                <p>Node JS</p>
+            </div>
+            <div className="durationContainer">
+            <p>2 months</p>
+            </div>
+            <div className="startDateContainer">
+            <p>11-09-2023</p>
+            </div>
+            <div className="endDateContainer">
+            <p>11-11-2023</p>
+            </div>
+            <div className="enrollContainer">
+                <button>Enroll</button>
+            </div>
+        </div>
+        <div className="batchCard">
+            <div className="courseImageContainer">
+                <img src="temp"/>
+            </div>
+            <div className="courseNameContainer">
+                <p>Node JS</p>
+            </div>
+            <div className="durationContainer">
+            <p>2 months</p>
+            </div>
+            <div className="startDateContainer">
+            <p>11-09-2023</p>
+            </div>
+            <div className="endDateContainer">
+            <p>11-11-2023</p>
+            </div>
+            <div className="enrollContainer">
+                <button>Enroll</button>
+            </div>
+        </div>
+        <div className="batchCard">
+            <div className="courseImageContainer">
+                <img src="temp"/>
+            </div>
+            <div className="courseNameContainer">
+                <p>Node JS</p>
+            </div>
+            <div className="durationContainer">
+            <p>2 months</p>
+            </div>
+            <div className="startDateContainer">
+            <p>11-09-2023</p>
+            </div>
+            <div className="endDateContainer">
+            <p>11-11-2023</p>
+            </div>
+            <div className="enrollContainer">
+                <button>Enroll</button>
+            </div>
+        </div>
+        <div className="batchCard">
+            <div className="courseImageContainer">
+                <img src="temp"/>
+            </div>
+            <div className="courseNameContainer">
+                <p>Node JS</p>
+            </div>
+            <div className="durationContainer">
+            <p>2 months</p>
+            </div>
+            <div className="startDateContainer">
+            <p>11-09-2023</p>
+            </div>
+            <div className="endDateContainer">
+            <p>11-11-2023</p>
+            </div>
+            <div className="enrollContainer">
+                <button>Enroll</button>
+            </div>
+        </div>
+
+        <h3 className='batchFooter'>Please feel free to reach us in case of any query. We are happy to help you.</h3>
+        </>
+    )
+}
+
+
+export default BatchComponent;
